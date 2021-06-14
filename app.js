a495a03ae2414c7032263462960a07bbbb35852d
IvanLezcano <lezcanoivangabriel@gmail.com>
2021-06-14 17:44:51 -0300

    primer commit, armando estructura

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const views = path.join(__dirname,"views")
+app.use(express.static("public"));
+console.log(__dirname);
+
+
+const port = process.env.PORT || 3030;
+
+
+app.get("/",(req,res) => res.sendFile(path.join(views,"index.html")))
+
+
+app.listen(port, () => {
+    console.log(`Server is running on the port ${port}.`);
+  });
+  
+  
+
+//npx nodemon app.js para ejecutarlo
